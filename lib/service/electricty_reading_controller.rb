b6862621a23c042e852a42b734bd5af674099080
Duncan Mortimer <dmortime@thoughtworks.com>
2018-01-26 18:05:39 +0000

    DM: electricity reading service can store and fetch readings

--- lib/service/electricty_reading_controller.rb
+++ lib/service/electricty_reading_controller.rb
@@ -1,5 +1,13 @@
 class ElectricityReadingService
+    def initialize()
+        @readings_store = Hash.new
+    end
+
     def getReadings(meter_id)
-        
+        @readings_store[meter_id]
+    end
+
+    def storeReadings(meter_id, readings)
+        @readings_store[meter_id] = readings
     end
 end
--- spec/service/electricity_reading_service_spec.rb
+++ spec/service/electricity_reading_service_spec.rb
@@ -8,4 +8,10 @@ describe ElectricityReadingService do
         expect(meter_reading_service.getReadings('nonexistent-meter')).to be_nil
     end
 
+    it "should return meter readings for a meter that exists" do
+        meter_reading_service = ElectricityReadingService.new
+        meter_reading_service.storeReadings('meter-id', [])
+        expect(meter_reading_service.getReadings('meter-id')).to eq []
+    end
+
 end
